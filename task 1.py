ae2af7f3a472a82cdc0d231134c297eea4ecf033
FentX141 <pozitron12340987@gmail.com>
2026-01-19 15:12:21 +0300

    Лабораторная работа 4

---
+++ task 1.py
@@ -0,0 +1,14 @@
+# TODO решите задачу
+import json
+def task() -> float:
+    with open('input.json') as json_file:
+        data = json.load(json_file)
+    total_sum=0
+    for item in data:
+        score = item["score"]
+        weight = item["weight"]
+        total_sum+=score*weight
+    return round(total_sum,3)
+
+
+print(task())
